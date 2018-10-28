a38a1c518f1338b59c10ff45e3998e003dca6b82
mauphes <mauphes@gmail.com>
2018-10-28 14:37:00 +0300

    Updates

---
+++ precache-manifest.cffba38bf9de423311985f8522d0f53b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "b5f5139b78fafdf93bd1",
+    "url": "/calculator/static/js/runtime~main.b5f5139b.js"
+  },
+  {
+    "revision": "a45a5e8b1a3f33314809",
+    "url": "/calculator/static/js/main.a45a5e8b.chunk.js"
+  },
+  {
+    "revision": "049ecd36bebe8315124e",
+    "url": "/calculator/static/js/1.049ecd36.chunk.js"
+  },
+  {
+    "revision": "a45a5e8b1a3f33314809",
+    "url": "/calculator/static/css/main.3f52b54c.chunk.css"
+  },
+  {
+    "revision": "409e2a5daa02ac7857d187c02db0bcba",
+    "url": "/calculator/index.html"
+  }
+];
